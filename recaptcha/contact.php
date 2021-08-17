41698f006de2c97161e43e0a91774edc76871eb4
msvchat <86755011+msvchat@users.noreply.github.com>
2021-08-17 11:45:28 -0500

    a

--- recaptcha/contact.php
+++ recaptcha/contact.php
@@ -23,7 +23,7 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // ReCaptch Secret
-$recaptchaSecret = '6LfKURIUAAAAAKEPdFXGUiRsQYtEYUnH1-OB5Mgx';
+$recaptchaSecret = '6LeRuwgcAAAAAMA0llwZL9oNSsk8QEWizUAWr2QL';
 
 // let's do the sending
 
