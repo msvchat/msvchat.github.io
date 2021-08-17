646014e5c81eb45190eb25dbd4c327f1176f62a0
msvchat <86755011+msvchat@users.noreply.github.com>
2021-08-17 13:50:25 -0500

    a

--- contact/contact.php
+++ contact/contact.php
@@ -4,7 +4,7 @@ require('recaptcha-master/src/autoload.php');
 
 // configure
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = '';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'BreadSmacker@outlook.com';
@@ -14,7 +14,7 @@ $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
