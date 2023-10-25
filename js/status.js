858305215fde30f810c97d22339a2465ccaae79c
MS V-Chat Museum <86755011+msvchat@users.noreply.github.com>
2023-10-25 03:04:18 -0500

    f

---
+++ js/status.js
@@ -0,0 +1,13 @@
+function isServerOnline(url) {
+    var script = document.createElement('script');
+    script.src = url + '?callback=checkServerStatus';
+    document.body.appendChild(script);
+  }
+  
+  function checkServerStatus(response) {
+    if (response.status === 'online') {
+      console.log('Server is online!');
+    } else {
+      console.log('Server is offline.');
+    }
+  }
