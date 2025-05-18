0c79036580ed6dcb61f891ef844d6441066e00c7
MS V-Chat Museum <msvchatmuseum@outlook.com>
2025-05-18 17:27:19 -0500

    ffgd

---
+++ assets/js/status2.js
@@ -0,0 +1,16 @@
+AbortSignal.timeout ??= function timeout(ms) {
+	const ctrl = new AbortController();
+	setTimeout(() => ctrl.abort(), ms);
+	return ctrl.signal;
+};
+fetch("https://msvchatsvr.webredirect.org:42422", {
+	signal: AbortSignal.timeout(2000), mode: "no-cors"
+})
+.then((r) => {
+	//console.log("ONLINE");
+	document.getElementById("status").innerHTML = "<a style=color:#4485b8>ONLINE</a>";
+})
+.catch((e) => {
+	//console.log("OFFLINE");
+	document.getElementById("status").innerHTML = "<a style=color:red>OFFLINE</a>";
+});
