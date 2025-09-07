f34436396f527514664653ea69ea06a34f8c333a
MS V-Chat Museum <msvchatmuseum@outlook.com>
2025-09-07 13:59:02 -0500

    kg

--- assets/js/status.js
+++ assets/js/status.js
@@ -3,7 +3,7 @@ AbortSignal.timeout ??= function timeout(ms) {
 	setTimeout(() => ctrl.abort(), ms);
 	return ctrl.signal;
 };
-fetch("https://msvchatsvr.webredirect.org", {
+fetch("https://msvchatsvr.webredirect.org:12443", {
 	signal: AbortSignal.timeout(2000), mode: "no-cors"
 })
 .then((r) => {
