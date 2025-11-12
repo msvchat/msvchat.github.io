ff0514c25d27ce75737206e8ded172b9c3d0cf69
MS V-Chat Museum <msvchatmuseum@outlook.com>
2025-11-12 00:25:49 -0600

    sdfas

--- assets/js/status.js
+++ assets/js/status.js
@@ -3,14 +3,14 @@ AbortSignal.timeout ??= function timeout(ms) {
 	setTimeout(() => ctrl.abort(), ms);
 	return ctrl.signal;
 };
-fetch("https://msvchatsvr.webredirect.org:12443", {
+fetch("https://msvchatsvr.webredirect.org:443", {
 	signal: AbortSignal.timeout(2000), mode: "no-cors"
 })
-.then((r) => {
-	//console.log("ONLINE");
-	document.getElementById("status").innerHTML = "<a style=color:#4485b8 href=vchat://msvchatsvr.webredirect.org/#V-ChatLobby>ONLINE</a>";
-})
-.catch((e) => {
-	//console.log("OFFLINE");
-	document.getElementById("status").innerHTML = "<a style=color:red>OFFLINE</a>";
-});
+	.then((r) => {
+		//console.log("ONLINE");
+		document.getElementById("status").innerHTML = "<a style=color:#4485b8 href=vchat://msvchatsvr.webredirect.org/#V-ChatLobby>ONLINE</a>";
+	})
+	.catch((e) => {
+		//console.log("OFFLINE");
+		document.getElementById("status").innerHTML = "<a style=color:red>OFFLINE</a>";
+	});
